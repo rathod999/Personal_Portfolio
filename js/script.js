6b9fd1a04445f84208f4eaadc983674bc20edbd3
rathod999 <rathodpraveen440@gmail.com>
2022-02-12 19:54:48 +0530

    navbar added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle Js
+const header = document.getElementById('header');
+const toggle = document.getElementById('toggle');
+const navbar = document.getElementById('navbar');
+
+document.onclick = function(e) {
+    if (e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar') {
+        navbar.classList.remove('active');
+        toggle.classList.remove('active');
+    }
+}
+
+toggle.onclick = function() {
+    toggle.classList.toggle('active');
+    navbar.classList.toggle('active');
+}
