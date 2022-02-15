902e791f9b3f475872aada54f27896b94d269f73
lazyprogrammer86 <adarshnayak86@gmail.com>
2022-02-14 19:43:04 -0800

    reverted to last good app state

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const header = document.getElementById('header');
 const toggle = document.getElementById('toggle');
 const navbar = document.getElementById('navbar');
 
-// document.onclick = function(e) {
-//     if (e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar') {
-//         navbar.classList.remove('active');
-//         toggle.classList.remove('active');
-//     }
-// }
+document.onclick = function(e) {
+    if (e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar') {
+        navbar.classList.remove('active');
+        toggle.classList.remove('active');
+    }
+}
 
 toggle.onclick = function() {
     toggle.classList.toggle('active');
@@ -18,23 +18,6 @@ toggle.onclick = function() {
 // image animation
 const text = document.qyuerySelector('.text p');
 text.innerHTML = text.innerText.split("").map((char, i) =>
-<<<<<<< HEAD
-<<<<<<< HEAD
     '<span style="transform:rotate(${i * 8.2}deg)"> ${char} < /span>').join("")
 
-<<<<<<< HEAD
-
-
-// Loading of page
-window.on('load', function() {
-    $('#loader').css
-})
-=======
 $('#auto_radius').circleType();
->>>>>>> b4eccd370f7c685ec4903ae4ec3b3ffea9cee292
-=======
-    '<span style="transform:rotate(${i * 8.2}deg)"> ${char} < /span>').join("")
->>>>>>> parent of b4eccd3... circle and menu changes added
-=======
-    '<span style="transform:rotate(${i * 8.2}deg)"> ${char} < /span>').join("")
->>>>>>> parent of b4eccd3... circle and menu changes added
