b3108bc0dc92badbe9ae767b7f95be13bec2657e
rathod999 <rathodpraveen440@gmail.com>
2022-02-15 13:41:55 +0530

    maded resp

--- js/script.js
+++ js/script.js
@@ -16,8 +16,3 @@ toggle.onclick = function() {
 }
 
 // image animation
-const text = document.qyuerySelector('.text p');
-text.innerHTML = text.innerText.split("").map((char, i) =>
-    '<span style="transform:rotate(${i * 8.2}deg)"> ${char} < /span>').join("")
-
-$('#auto_radius').circleType();
