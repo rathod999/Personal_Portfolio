87df647d7fd3ffb4b662f939d9fa72ed34710ff0
rathod999 <rathodpraveen440@gmail.com>
2022-03-05 19:35:39 +0530

    added homepage image

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const header = document.getElementById('header');
 const toggle = document.getElementById('toggle');
 const navbar = document.getElementById('navbar');
 
-document.onclick = function(e) {
-    if (e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar') {
-        navbar.classList.remove('active');
-        toggle.classList.remove('active');
-    }
-}
+// document.onclick = function(e) {
+//     if (e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar') {
+//         navbar.classList.remove('active');
+//         toggle.classList.remove('active');
+//     }
+// }
 
 toggle.onclick = function() {
     toggle.classList.toggle('active');
